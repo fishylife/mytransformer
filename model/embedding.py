ae5bacc9a13314945b2e869c98c21bcddf1bc6b7
HuiCheng Huang <fishlife.mic@gmail.com>
2026-07-30 02:51:34 +0000

    embedding

---
+++ model/embedding.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+import math
+import torch
+
+class embedding(nn.Module):
+    def __init__(self, d_model, vocab_size):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        self.d_model = d_model
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+
+#test
+embedding_layer = embedding(d_model=512, vocab_size=10000)
+x = torch.randint(0, 10000, (1, 10))  # Example input tensor with shape (batch_size, sequence_length)
+print(x.shape)  # Should print torch.Size([1, 10])
+output = embedding_layer(x)
+print(output.shape)  # Should print torch.Size([1, 10, 512])
